f5d9881284bcb371ca2e64b377d9015ea0b35ecf
Christopher Giordano <chrisgcpg@gmail.com>
2024-04-13 21:11:34 -0400

    Added functionality to the buttons on the page by adding an event listener to each button and running the function with the button has a parameter.

--- Assets/scripts/index-script.js
+++ Assets/scripts/index-script.js
@@ -1,7 +1,7 @@
 // Selects the form element in the html.
 const searchForm = document.querySelector('#search-form');
 const searchButton = document.querySelector('#search-button');
-const cityButtons = document.querySelector('#city-buttons button');
+const cityButtons = document.querySelectorAll('.city-button');
 const cardsSection = document.querySelector('#cards');
 // variable for my api key.
 const apiKey = 'b52d0dab464b966c275ec23279d9be7d';
@@ -10,6 +10,7 @@ const apiKey = 'b52d0dab464b966c275ec23279d9be7d';
 // function to get the coordinates of the city and place them as parameters into the forcast url.
 function handleSearchForm(event) {
     event.preventDefault();
+    cardsSection.innerHTML = '';
 
     const cityName = searchForm.searchInput.value;
     console.log(`City Name: ${cityName}`);
@@ -50,6 +51,53 @@ function handleSearchForm(event) {
     // API to find the weather data based on the lon/lat parameters.
 
 }
+// Adds event listener to each button on the page.
+cityButtons.forEach(cityButton =>{
+
+    cityButton.addEventListener('click', function(){
+        // Clears the weather data
+        cardsSection.innerHTML = '';
+
+        cityButton = cityButton.textContent;
+        console.log(`City Name: ${cityButton}`);
+        console.log(typeof cityButton);
+    
+        // Selects the input value
+        // Alerts if no input was made.
+        if (cityButton === undefined) {
+            window.alert("Please enter a city name");
+            return
+        }
+          // API to find the coordinates of the city.
+          const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityButton}&appid=${apiKey}`;
+    
+          // Fetch data from the weatherAPI
+          fetch(weatherUrl)
+              .then(function (response) {
+                  if (!response.ok) {
+                      throw response.json();
+                  }
+                  return response.json();
+              })
+      
+              // Variables for the latitude/longitude.
+              .then(function (data) {
+                  let lat = data.city.coord.lat;
+                  let lon = data.city.coord.lon;
+                  console.log(lat);
+                  console.log(lon);
+                  const forcastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+                  getWeatherData(forcastUrl);
+              })
+              .catch(function (error) {
+                  console.error('Error fetching city coordinates:', error);
+              });
+    });
+});
+function handleCityButtons(cityButton){
+   
+}
+
 function getWeatherData(forcastUrl) {
     fetch(forcastUrl)
         .then(function (response) {
@@ -61,14 +109,14 @@ function getWeatherData(forcastUrl) {
         // Access data and append to the document
         .then(function (data) {
             console.log(data);
-
+            
             const dailyCard = document.createElement('div');
             dailyCard.classList.add('daily-card');
 
             const dailyCity = document.createElement('h2');
             dailyCity.classList.add('daily-city');
             dailyCity.textContent = data.city.name;
-
+            
             const dailyDay = document.createElement('h3');
             dailyDay.classList.add('daily-day');
             // dailyDay.textContent = dayjs.unix(data.list[0].dt).format("dddd");
@@ -86,11 +134,11 @@ function getWeatherData(forcastUrl) {
             const dailyHumidity = document.createElement('p');
             dailyHumidity.classList.add('daily-Humidity');
             dailyHumidity.textContent = `Humidity: ${data.list[0].main.humidity}`;
-
+            
             const dailyTemp = document.createElement('p');
             dailyTemp.classList.add('daily-Temp');
             dailyTemp.textContent = `Temperature: ${data.list[0].main.temp}F\u00B0`;
-
+            
             const dailyWind = document.createElement('p');
             dailyWind.classList.add('daily-Wind');
             dailyWind.textContent = `Wind Speed: ${data.list[0].wind.speed}MPH`;
@@ -104,46 +152,46 @@ function getWeatherData(forcastUrl) {
             forecastTitle.textContent = "5 Day forecast";
             cardsSection.append(forecastTitle);
             // Loop thru 5 days
-            for (i = 7; i < data.list.length; i += 8) {
+            for(i = 7; i < data.list.length; i += 8) {
                 const forecastCard = document.createElement('div');
-                forecastCard.classList.add('forecast-card');
+            forecastCard.classList.add('forecast-card');
 
-                // const forecastCity = document.createElement('h2');
-                // forecastCity.classList.add('forecast-name');
-                // forecastCity.textContent = data.city.name;
+            // const forecastCity = document.createElement('h2');
+            // forecastCity.classList.add('forecast-name');
+            // forecastCity.textContent = data.city.name;
 
 
-                const forecastDay = document.createElement('h3');
-                forecastDay.classList.add('forecast-date');
-                forecastDay.textContent = dayjs.unix(data.list[i].dt).format("dddd");
+            const forecastDay = document.createElement('h3');
+            forecastDay.classList.add('forecast-date');
+            forecastDay.textContent = dayjs.unix(data.list[i].dt).format("dddd");
 
-                const forecastDate = document.createElement('p');
-                forecastDate.classList.add('forecast-date');
-                forecastDate.textContent = dayjs.unix(data.list[i].dt).format("MMMM DD, YYYY");
+            const forecastDate = document.createElement('p');
+            forecastDate.classList.add('forecast-date');
+            forecastDate.textContent = dayjs.unix(data.list[i].dt).format("MMMM DD, YYYY");
 
-                const forecastIcon = document.createElement('img');
-                forecastIcon.classList.add('forecast-icon');
-                iconSource = data.list[i].weather[0].icon
-                forecastIcon.src = `https://openweathermap.org/img/wn/${iconSource}@2x.png`;
+            const forecastIcon = document.createElement('img');
+            forecastIcon.classList.add('forecast-icon');
+            iconSource = data.list[i].weather[0].icon
+            forecastIcon.src = `https://openweathermap.org/img/wn/${iconSource}@2x.png`;
 
-                const forecastHumidity = document.createElement('p');
-                forecastHumidity.classList.add('forecast-Humidity');
-                forecastHumidity.textContent = `Humidity: ${data.list[i].main.humidity}`;
+            const forecastHumidity = document.createElement('p');
+            forecastHumidity.classList.add('forecast-Humidity');
+            forecastHumidity.textContent = `Humidity: ${data.list[i].main.humidity}`;
+            
+            const forecastTemp = document.createElement('p');
+            forecastTemp.classList.add('forecast-Temp');
+            forecastTemp.textContent = `Temperature: ${data.list[i].main.temp}F\u00B0`;
+            
+            const forecastWind = document.createElement('p');
+            forecastWind.classList.add('forecast-Wind');
+            forecastWind.textContent = `Wind Speed: ${data.list[i].wind.speed}MPH`;
 
-                const forecastTemp = document.createElement('p');
-                forecastTemp.classList.add('forecast-Temp');
-                forecastTemp.textContent = `Temperature: ${data.list[i].main.temp}F\u00B0`;
+0
+            forecastCard.append(forecastDay, forecastDate, forecastIcon, forecastHumidity, forecastTemp, forecastWind);
 
-                const forecastWind = document.createElement('p');
-                forecastWind.classList.add('forecast-Wind');
-                forecastWind.textContent = `Wind Speed: ${data.list[i].wind.speed}MPH`;
-
-                0
-                forecastCard.append(forecastDay, forecastDate, forecastIcon, forecastHumidity, forecastTemp, forecastWind);
-
-                cardsSection.append(forecastCard);
+            cardsSection.append(forecastCard);
             }
-
+            
 
 
         })
@@ -152,13 +200,6 @@ function getWeatherData(forcastUrl) {
         });
 }
 
-function inputCityButton(event) {
-    const searchInput = document.getElementById('search-input');
-    searchInput.textContent = cityButtons.textContent;
-    handleSearchForm(event);
-}
-
-cityButtons.addEventListener('click', inputCityButton);
 
 searchButton.addEventListener('click', handleSearchForm);
 
